b5de582cebcf14f7fae49a4f357efaa27a3603c3
kawkab <61437916+kawkb@users.noreply.github.com>
2022-06-02 09:29:39 +0100

    Update solution.py

--- level3/Fuel_Injection_Perfection/solution.py
+++ level3/Fuel_Injection_Perfection/solution.py
@@ -2,9 +2,7 @@ def solution(n):
     num = int(n)
     i = 0
     while(num != 1):
-        if num == 1:
-            return i
-        elif num % 2 == 0:
+        if num % 2 == 0:
             num = num // 2
         else:
             if ( ((num - 1) % 2 == 0 and ((num - 1) // 2) % 2 == 0) or num - 1 == 2):
